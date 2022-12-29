7ae3967f283683a689116487278f1f6615f89eb3
João <joaopedronk22@gmail.com>
2022-12-28 21:45:49 -0300

    First global stylings and also app header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
-import type { AppProps } from 'next/app'
-import { globalStyles } from '../styles/global'
+import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+import logoImg from "../assets/logo.svg";
+import Image from "next/image";
+import { Container, Header } from "../styles/pages/app";
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image
+          src={logoImg.src}
+          alt="Logo IgniteShop"
+          title="Logo IgniteShop"
+          width={logoImg.width}
+          height={logoImg.height}
+        />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
