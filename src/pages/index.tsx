bdf7f5d15f0186e8daea07c734f1c06ad8e96104
João <joaopedronk22@gmail.com>
2022-12-28 22:29:27 -0300

    Home page styling and setting default font-sizes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,40 @@
-import { styled } from "../styles"
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight:'bold'
-  },
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-});
+import camisa01 from '../assets/camisetas/camisa01.png';
+import camisa02 from '../assets/camisetas/camisa02.png';
+import camisa03 from '../assets/camisetas/camisa03.png';
 
 export default function Home(){
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image 
+          src={camisa01}
+          width={520}
+          height={480}
+          alt="Camisa 01"
+          title="Camisa 01"
+        />
+
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,00</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image 
+          src={camisa02}
+          width={520}
+          height={480}
+          alt="Camisa 02"
+          title="Camisa 02"
+        />
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,00</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
